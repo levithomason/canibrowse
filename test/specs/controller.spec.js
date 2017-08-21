d389e98b265a38945e880050b9af02822add1d9e
Levi Thomason <me@levithomason.com>
2017-08-21 14:40:01 -0700

    feat(query): add mobile and tablet flags

--- test/specs/controller.spec.js
+++ test/specs/controller.spec.js
@@ -5,28 +5,36 @@ const userAgents = require('../fixtures/userAgents')
 describe('controller', () => {
   describe('isBrowserSupported', () => {
     describe('IE >= 9', () => {
-      const opts = { browsers: { msie: '9' } }
+      const opts = { browsers: { msie: { minVersions: ['9'] } } }
 
       test('IE 8 cannot browse', () => {
         expect(isBrowserSupported(userAgents.msie8, opts)).toMatchObject({ canBrowse: false })
       })
+
       test('IE 9 can browse', () => {
         expect(isBrowserSupported(userAgents.msie9, opts)).toMatchObject({ canBrowse: true })
       })
+
       test('IE 11 can browse', () => {
         expect(isBrowserSupported(userAgents.msie11, opts)).toMatchObject({ canBrowse: true })
       })
+
       test('Non IE can browse', () => {
         expect(isBrowserSupported(userAgents.chrome51Mac, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.firefox46Win, opts)).toMatchObject({ canBrowse: true })
-        expect(isBrowserSupported(userAgents.safari10, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: true })
       })
     })
 
     describe('Only Chrome and Firefox', () => {
-      const opts = { strict: true, browsers: { chrome: true, firefox: true } }
+      const opts = {
+        strict: true,
+        browsers: { chrome: { whitelisted: true }, firefox: { whitelisted: true } },
+      }
 
       test('Chrome can browse', () => {
         expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: true })
@@ -34,21 +42,27 @@ describe('controller', () => {
         expect(isBrowserSupported(userAgents.chrome58Mac, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.chrome60Mac, opts)).toMatchObject({ canBrowse: true })
       })
+
       test('Firefox can browse', () => {
         expect(isBrowserSupported(userAgents.firefox46Win, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({ canBrowse: true })
       })
+
       test('Other browsers cannot browse', () => {
         expect(isBrowserSupported(userAgents.msie8, opts)).toMatchObject({ canBrowse: false })
         expect(isBrowserSupported(userAgents.msie9, opts)).toMatchObject({ canBrowse: false })
         expect(isBrowserSupported(userAgents.msie11, opts)).toMatchObject({ canBrowse: false })
         expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: false })
-        expect(isBrowserSupported(userAgents.safari10, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({
+          canBrowse: false,
+        })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: false })
       })
     })
 
     describe('Blacklist specific browsers', () => {
-      const opts = { browsers: { msie: false } }
+      const opts = { debug: true, browsers: { msie: { blacklisted: true } } }
 
       test('IE cannot browse', () => {
         expect(isBrowserSupported(userAgents.msie8, opts)).toMatchObject({ canBrowse: false })
@@ -64,12 +78,14 @@ describe('controller', () => {
         expect(isBrowserSupported(userAgents.firefox46Win, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({ canBrowse: true })
         expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: true })
-        expect(isBrowserSupported(userAgents.safari10, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: true })
       })
     })
 
     describe('Whitelist specific browsers', () => {
-      const opts = { strict: true, browsers: { chrome: true } }
+      const opts = { strict: true, browsers: { chrome: { whitelisted: true } } }
 
       test('Chrome can browse', () => {
         expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: true })
@@ -89,7 +105,131 @@ describe('controller', () => {
         expect(isBrowserSupported(userAgents.msie8, opts)).toMatchObject({ canBrowse: false })
         expect(isBrowserSupported(userAgents.msie9, opts)).toMatchObject({ canBrowse: false })
         expect(isBrowserSupported(userAgents.msie11, opts)).toMatchObject({ canBrowse: false })
-        expect(isBrowserSupported(userAgents.safari10, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({
+          canBrowse: false,
+        })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: false })
+      })
+    })
+
+    describe('Strictly mobile', () => {
+      const opts = { strict: true, mobile: true }
+
+      test('Mobile can browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: true })
+      })
+
+      test('Tablet cannot browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({
+          canBrowse: false,
+        })
+      })
+
+      test('Desktop browsers cannot browse', () => {
+        expect(isBrowserSupported(userAgents.chrome51Mac, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.chrome58Mac, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.chrome60Mac, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({
+          canBrowse: false,
+        })
+        expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: false })
+      })
+    })
+
+    describe('Strictly tablet', () => {
+      const opts = { strict: true, tablet: true, debug: true }
+
+      test('Mobile cannot browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: false })
+      })
+
+      test('Tablet can browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({ canBrowse: true })
+      })
+
+      test('Desktop cannot browse', () => {
+        expect(isBrowserSupported(userAgents.chrome51Mac, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.chrome58Mac, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.chrome60Mac, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({
+          canBrowse: false,
+        })
+        expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: false })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: false })
+      })
+    })
+
+    describe('Strictly desktop', () => {
+      const opts = { strict: true, mobile: false, tablet: false }
+
+      test('Mobile cannot browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: false })
+      })
+
+      test('Tablet cannot browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({
+          canBrowse: false,
+        })
+      })
+
+      test('Desktop browsers can browse', () => {
+        expect(isBrowserSupported(userAgents.chrome51Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome58Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome60Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: true })
+      })
+    })
+
+    describe('Exclude mobile', () => {
+      const opts = { mobile: false }
+
+      test('Mobile cannot browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: false })
+      })
+
+      test('Tablet can browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({ canBrowse: true })
+      })
+
+      test('Desktop browsers can browse', () => {
+        expect(isBrowserSupported(userAgents.chrome51Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome58Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome60Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: true })
+      })
+    })
+
+    describe('Exclude tablet', () => {
+      const opts = { tablet: false }
+
+      test('Mobile can browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iOS, opts)).toMatchObject({ canBrowse: true })
+      })
+
+      test('Tablet cannot browse', () => {
+        expect(isBrowserSupported(userAgents.safari10iPad, opts)).toMatchObject({
+          canBrowse: false,
+        })
+      })
+
+      test('Desktop browsers can browse', () => {
+        expect(isBrowserSupported(userAgents.chrome51Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome58Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome60Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.chrome51Win, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.firefox46Mac, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.msedge13, opts)).toMatchObject({ canBrowse: true })
+        expect(isBrowserSupported(userAgents.safari10Mac, opts)).toMatchObject({ canBrowse: true })
       })
     })
   })
--- test/specs/handlers.spec.js
+++ test/specs/handlers.spec.js
@@ -1,22 +1,119 @@
+const _ = require('lodash')
 const HTTPError = require('node-http-error')
 
-const { getRoot } = require('../../src/handlers')
+const { getRoot, requestToArguments } = require('../../src/handlers')
 const userAgents = require('../fixtures/userAgents')
 
-const makeRequest = (obj = {}) =>
-  Object.assign({}, obj, { header: () => {}, query: Object.assign({}, obj.query) })
+const DEFAULT_REQ = { header: () => {}, query: {} }
+const DEFAULT_RES = { json: () => {} }
+
+const req = (req = {}) => _.merge({}, DEFAULT_REQ, req)
+const res = (res = {}) => _.merge({}, DEFAULT_RES, res)
 
 describe('handlers', () => {
+  describe('requestToArguments', () => {
+    test('coerces boolean query params', () => {
+      const input = req({ query: { debug: 'true', mobile: 'false', strict: '', tablet: 'false' } })
+      const expected = { debug: true, mobile: false, strict: true, tablet: false }
+
+      expect(requestToArguments(input)).toMatchObject(expected)
+    })
+
+    test('defaults ua to the User-Agent header', () => {
+      const mockRequest = req({ header: name => name })
+      const { ua } = requestToArguments(mockRequest)
+
+      expect(ua).toEqual('User-Agent')
+    })
+
+    test('returns top level keys: debug, mobile, strict, tablet', () => {
+      const args = requestToArguments(
+        req({ query: { debug: '', mobile: '', strict: '', tablet: '', ua: 'foo' } }),
+      )
+
+      expect(args).toMatchObject({ debug: true, mobile: true, strict: true, tablet: true })
+    })
+
+    test('does not return undefined keys', () => {
+      const args = requestToArguments(req())
+
+      Object.keys(args).forEach(key => {
+        // assert the object doesn't have the property for better failure messages
+        if (args[key] === undefined) expect(args).not.toHaveProperty(key)
+      })
+    })
+
+    describe('browser specifications', () => {
+      test('considers extra query params to be browser specifications', () => {
+        const args = requestToArguments(req({ query: { aBrowser: '0.1' } }))
+
+        expect(args).toHaveProperty('browsers.aBrowser')
+      })
+
+      test('handles whitelisting with "true"', () => {
+        const args = requestToArguments(req({ query: { chrome: 'true' } }))
+
+        expect(args).toHaveProperty('browsers.chrome', {
+          blacklisted: false,
+          minVersions: [],
+          whitelisted: true,
+        })
+      })
+
+      test("handles whitelisting with ''", () => {
+        const args = requestToArguments(req({ query: { firefox: '' } }))
+
+        expect(args).toHaveProperty('browsers.firefox', {
+          blacklisted: false,
+          minVersions: [],
+          whitelisted: true,
+        })
+      })
+
+      test('handles blacklisting', () => {
+        const args = requestToArguments(req({ query: { msie: 'false' } }))
+
+        expect(args).toHaveProperty('browsers', {
+          msie: {
+            blacklisted: true,
+            minVersions: [],
+            whitelisted: false,
+          },
+        })
+      })
+
+      test('handles single versions', () => {
+        const args = requestToArguments(req({ query: { chrome: '60' } }))
+
+        expect(args).toHaveProperty('browsers.chrome', {
+          blacklisted: false,
+          minVersions: ['60'],
+          whitelisted: false,
+        })
+      })
+
+      test('handles multiple versions', () => {
+        const args = requestToArguments(req({ query: { msie: ['10', '11'] } }))
+
+        expect(args).toHaveProperty('browsers.msie', {
+          blacklisted: false,
+          minVersions: ['10', '11'],
+          whitelisted: false,
+        })
+      })
+    })
+  })
+
   describe('getRoot', () => {
     test('throws if missing req.query.ua', () => {
-      expect(() => getRoot(makeRequest())).toThrow(
-        new HTTPError('Missing required `ua` query param. See docs https://goo.gl/RJGJgf.'),
+      expect(() => getRoot(req(), res())).toThrow(
+        new HTTPError(400, 'Missing required `ua` query param. Docs https://goo.gl/RJGJgf.'),
       )
     })
 
     test('throws if no browsers were specified ', () => {
-      expect(() => getRoot(makeRequest({ query: { ua: userAgents.chrome51Mac } }))).toThrow(
-        new HTTPError('No browsers were specified. See docs https://goo.gl/RJGJgf.'),
+      expect(() => getRoot(req({ query: { ua: userAgents.chrome51Mac } }), res())).toThrow(
+        new HTTPError(400, 'Request has no browser requirements. Docs https://goo.gl/RJGJgf.'),
       )
     })
   })
